187d72ade1a8f27bf45fc65110dfb2b8960b15e0
nikolinehalbak <128796372+nikoUCN@users.noreply.github.com>
2023-12-05 15:23:23 +0100

    validateLogin flyttet til EmployeeController

--- 2ndSemesterProject/src/Control/EmployeeController.java
+++ 2ndSemesterProject/src/Control/EmployeeController.java
@@ -2,6 +2,10 @@ package Control;
 
 import Model.Employee;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import Database.EmployeeDAO;
 
 public class EmployeeController {
@@ -16,11 +20,27 @@ public class EmployeeController {
 		Employee employee = findEmployeeById(id);
 
 		if (employee != null) {
-			String enteredPassword = employeeDAO.hashPassword(password);
+			String enteredPassword = hashPassword(password);
 			if (enteredPassword.equals(employee.getPassword())) {
 				return true;
 			}
 		}
 		return false;
+	}
+	
+	public String hashPassword(String password) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+			StringBuilder sb = new StringBuilder();
+
+			for (byte b : hashedPassword) {
+				sb.append(String.format("%02x", b));
+			}
+			return sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
--- 2ndSemesterProject/src/Database/EmployeeDAO.java
+++ 2ndSemesterProject/src/Database/EmployeeDAO.java
@@ -1,8 +1,5 @@
 package Database;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -43,22 +40,6 @@ public class EmployeeDAO {
 			e.printStackTrace();
 		}
 		return employee;
-	}
-	
-	public String hashPassword(String password) {
-		try {
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
-			StringBuilder sb = new StringBuilder();
-
-			for (byte b : hashedPassword) {
-				sb.append(String.format("%02x", b));
-			}
-			return sb.toString();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			return null;
-		}
 	}
 
 }
