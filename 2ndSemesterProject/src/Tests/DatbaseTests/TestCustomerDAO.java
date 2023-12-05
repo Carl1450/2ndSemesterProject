8eaa59f0a0062011376512ebbbf48e3db5e2433c
RadenDK <128796441+RasmusJensenUCN@users.noreply.github.com>
2023-12-05 11:18:24 +0100

    Update tests
    Made it so that the tests insert and deletes mockdata

--- 2ndSemesterProject/src/Tests/DatbaseTests/TestCustomerDAO.java
+++ 2ndSemesterProject/src/Tests/DatbaseTests/TestCustomerDAO.java
@@ -2,12 +2,64 @@ package Tests.DatbaseTests;
 
 import Database.ConnectionEnvironment;
 import Database.CustomerDAO;
+import Database.DBConnection;
 import Model.Customer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCustomerDAO {
+
+    @BeforeEach
+    void setUp() {
+        Connection connection = DBConnection.getInstance(ConnectionEnvironment.TESTING).getConnection();
+
+        // Insert a mock city
+        String mockCityInsertQuery = "INSERT INTO City (zipCode, city) VALUES (1000, 'Copenhagen');";
+
+        // Insert a mock address that references the mock city
+        String mockAddressInsertQuery = "INSERT INTO [Address] (id, street, streetno, zipcode) VALUES (1, 'Bredgade', 30, 1000);";
+
+        // Insert a mock customer that references the mock address
+        String mockCustomerInsertQuery =
+                "INSERT INTO Customer (id, fname, lname, email, phoneno, addressId) VALUES " +
+                        "(1, 'Jens', 'Hansen', 'jens.hansen@example.com', '+45 12345678', 1);";
+
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(mockCityInsertQuery);
+            statement.executeUpdate(mockAddressInsertQuery);
+            statement.executeUpdate(mockCustomerInsertQuery);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error setting up mock data", e);
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        Connection connection = DBConnection.getInstance(ConnectionEnvironment.TESTING).getConnection();
+
+        // Delete in reverse order of creation
+        String deleteMockDataQuery =
+                "DELETE FROM Customer WHERE id = 1; " +
+                        "DELETE FROM [Address] WHERE id = 1; " +
+                        "DELETE FROM City WHERE zipCode = 1000;";
+
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(deleteMockDataQuery);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error cleaning up mock data", e);
+        }
+    }
+
+
     @Test
     void TS_2_TC_1_find_customer_by_phone_number_with_valid_number() {
         // Arrange
