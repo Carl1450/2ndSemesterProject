e6ddf4ad923d6dd1b6d204b07e1579a3f0dc2be5
RadenDK <128796441+RasmusJensenUCN@users.noreply.github.com>
2023-12-06 23:05:18 +0100

    Tried better structureing of queries introducing transactions and reservations in savebooking

--- 2ndSemesterProject/src/Control/BookingController.java
+++ 2ndSemesterProject/src/Control/BookingController.java
@@ -1,10 +1,13 @@
 package Control;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import Database.BookingDAO;
+import Database.DBConnection;
 import Model.Booking;
 import Model.Campsite;
 import Model.Customer;
@@ -16,48 +19,105 @@ public class BookingController {
     private Booking currentBooking;
     private CustomerController customerController;
 
+    private Connection bookingConnection;
+
     public BookingController() {
         customerController = new CustomerController();
         campsiteController = new CampsiteController();
 
-        BookingDAO bookingDAO = new BookingDAO();
+        bookingDAO = new BookingDAO();
+
     }
 
     public Booking startBooking(Employee employee) {
         currentBooking = new Booking();
         return currentBooking;
+    }
 
+    public Customer findCustomerByPhoneNumber(String phoneNumber) {
+        Customer customer = customerController.findCustomerByPhoneNumber(phoneNumber);
+        currentBooking.setCustomer(customer);
+        return customer;
     }
 
     public List<Campsite> getAvailableCampsites(String startDate, String endDate) {
         return campsiteController.getAvailableCampsites(Date.valueOf(startDate), Date.valueOf(endDate));
     }
 
-    public void addCampsiteToBooking(Campsite campsite) {
-        currentBooking.setCampsite(campsite);
+    public void addCampsiteToBooking(Campsite campsite, Date startDate, Date endDate) {
+        if (reserveCampsite(campsite, startDate, endDate)) {
+            currentBooking.setCampsite(campsite);
+        }
     }
 
-    public void addCustomerToBooking(Customer customer) {
-        currentBooking.setCustomer(customer);
-    }
+    private boolean reserveCampsite(Campsite campsite, Date startDate, Date endDate) {
+        boolean reservationSuccess = false;
 
-    public Customer findCustomerByPhoneNumber(String phoneNumber) {
-        return customerController.findCustomerByPhoneNumber(phoneNumber);
+        try {
+            bookingConnection = DBConnection.getInstance().getConnection();
+            bookingConnection.setAutoCommit(false);
 
-    }
+            reservationSuccess = campsiteController.reserveCampsite(campsite, startDate, endDate, currentBooking.getEmployee().getId());
 
-    public Booking getCurrentBooking() {
-        return currentBooking;
-
-    }
-
-    public boolean reserveCampsite(Campsite campsite, Date startDate, Date endDate, Employee employee) {
-        return campsiteController.reserveCampsite(campsite, startDate, endDate, employee);
+            if (!reservationSuccess) {
+                bookingConnection.rollback();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            if (bookingConnection != null) {
+                try {
+                    bookingConnection.rollback();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        return reservationSuccess;
     }
 
     public boolean saveBooking(Booking booking) {
-        // TODO remember to relese/invalidate the reservation in the DB made from reserve campsite
-        return bookingDAO.saveBooking(booking);
+        boolean bookingSuccess = false;
+        try {
+            if (bookingConnection != null && !bookingConnection.getAutoCommit()) {
+                bookingSuccess = bookingDAO.saveBooking(booking);
+                if (bookingSuccess) {
+                    bookingConnection.commit();
+                } else {
+                    bookingConnection.rollback();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            if (bookingConnection != null) {
+                try {
+                    bookingConnection.rollback();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            bookingSuccess = false;
+        } finally {
+            if (bookingConnection != null) {
+                try {
+                    bookingConnection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bookingSuccess;
+    }
+
+
+    public void cancelBooking() {
+        try {
+            if (bookingConnection != null && !bookingConnection.getAutoCommit()) {
+                bookingConnection.rollback();
+                bookingConnection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -66,4 +126,8 @@ public class BookingController {
 
     }
 
+    public Booking getCurrentBooking() {
+        return currentBooking;
+
+    }
 }
