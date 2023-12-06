a66b574e2c626adf7220d458c6ae4d5835a67d21
RadenDK <128796441+RasmusJensenUCN@users.noreply.github.com>
2023-12-06 13:59:44 +0100

    Update BookingController.java

--- 2ndSemesterProject/src/Control/BookingController.java
+++ 2ndSemesterProject/src/Control/BookingController.java
@@ -11,59 +11,59 @@ import Model.Customer;
 import Model.Employee;
 
 public class BookingController {
-	private BookingDAO bookingDAO;
-	private CampsiteController campsiteController;
-	private Booking currentBooking;
-	private CustomerController customerController;
+    private BookingDAO bookingDAO;
+    private CampsiteController campsiteController;
+    private Booking currentBooking;
+    private CustomerController customerController;
 
-	public BookingController() {
-		customerController = new CustomerController();
-		campsiteController = new CampsiteController();
+    public BookingController() {
+        customerController = new CustomerController();
+        campsiteController = new CampsiteController();
 
-		BookingDAO bookingDAO = new BookingDAO();
-	}
+        BookingDAO bookingDAO = new BookingDAO();
+    }
 
-	public Booking startBooking(Employee employee) {
-		currentBooking = new Booking();
-		return currentBooking;
+    public Booking startBooking(Employee employee) {
+        currentBooking = new Booking();
+        return currentBooking;
 
-	}
+    }
 
-	public List<Campsite> getAvailableCampsites(Date startDate, Date endDate) {
+    public List<Campsite> getAvailableCampsites(String startDate, String endDate) {
+        return campsiteController.getAvailableCampsites(Date.valueOf(startDate), Date.valueOf(endDate));
+    }
 
-		return campsiteController.getAvailebleCampsites(startDate, endDate);
-	}
+    public void addCampsiteToBooking(Campsite campsite) {
+        currentBooking.setCampsite(campsite);
+    }
 
-	public void addCampsiteToBooking(Campsite campsite) {
-		currentBooking.setCampsite(campsite);
-	}
+    public void addCustomerToBooking(Customer customer) {
+        currentBooking.setCustomer(customer);
+    }
 
-	public void addCustomerToBooking(Customer customer) {
-		currentBooking.setCustomer(customer);
-	}
+    public Customer findCustomerByPhoneNumber(String phoneNumber) {
+        return customerController.findCustomerByPhoneNumber(phoneNumber);
 
-	public Customer findCustomerByPhoneNumber(String phoneNumber) {
-		return customerController.findCustomerByPhoneNumber(phoneNumber);
+    }
 
-	}
+    public Booking getCurrentBooking() {
+        return currentBooking;
 
-	public Booking getCurrentBooking() {
-		return currentBooking;
+    }
 
-	}
+    public boolean reserveCampsite(Campsite campsite, Date startDate, Date endDate, Employee employee) {
+        return campsiteController.reserveCampsite(campsite, startDate, endDate, employee);
+    }
 
-	public boolean reserveCampsite(Campsite campsite, Date startDate, Date endDate, Employee employee) {
-		return campsiteController.reserveCampsite(campsite, startDate, endDate, employee);
-	}
+    public boolean saveBooking(Booking booking) {
+        // TODO remember to relese/invalidate the reservation in the DB made from reserve campsite
+        return bookingDAO.saveBooking(booking);
+    }
 
-	public boolean saveBooking(Booking booking) {
-		// TODO remember to relese/invalidate the reservation in the DB made from reserve campsite
-		return bookingDAO.saveBooking(booking);
-	}
 
-	public boolean handlePayment(float amount) {
-		return amount > 0;
+    public boolean handlePayment(float amount) {
+        return amount > 0;
 
-	}
+    }
 
 }
