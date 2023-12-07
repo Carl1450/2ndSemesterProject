d8c7e3ac7c7f49adb8a627ea01107f91689031a9
xgree <xgree@DESKTOP-5BFV3VF>
2023-12-07 18:37:01 +0100

    Review of TestEmployeeDAO

--- 2ndSemesterProject/src/Tests/DatbaseTests/TestEmployeeDAO.java
+++ 2ndSemesterProject/src/Tests/DatbaseTests/TestEmployeeDAO.java
@@ -22,16 +22,20 @@ class TestEmployeeDAO {
     void setUp() {
         Connection connection = DBConnection.getInstance(ConnectionEnvironment.TESTING).getConnection();
         String mockAddressInsertQuery = "INSERT INTO [Address] (id, street, streetno, zipcode) VALUES (1, 'Bredgade', 30, 1000);";
+        
         String mockCityInsertQuery = "INSERT INTO City (zipCode, city) VALUES (1000, 'Copenhagen');";
 
         String mockEmployeeInsertQuery = "INSERT INTO [Employee] (id, fname, lname, email, phoneno, role, cprno, Password, addressid) "
                 + "VALUES (1,'kurt', 'Den' ,'ansat@mail.com', '+45 22258181','admin', 11198-4575, 'password1','1');";
         try {
             Statement statement = connection.createStatement();
+            
             statement.executeUpdate(mockCityInsertQuery);
+            
             statement.executeUpdate("SET IDENTITY_INSERT [Address] ON");
             statement.executeUpdate(mockAddressInsertQuery);
             statement.executeUpdate("SET IDENTITY_INSERT [Address] OFF");
+            
             statement.executeUpdate("SET IDENTITY_INSERT [Employee] ON");
             statement.executeUpdate(mockEmployeeInsertQuery);
             statement.executeUpdate("SET IDENTITY_INSERT [Employee] OFF");
@@ -42,48 +46,7 @@ class TestEmployeeDAO {
         }
 
     }
-
-    @Test
-    void TS_4_TC_1_find_Employee_by_id_with_valid_id() {
-        // Arrange
-        EmployeeDAO SUT = new EmployeeDAO(ConnectionEnvironment.TESTING);
-        String id = "1";
-        int idInt = Integer.parseInt(id);
-
-        // Act
-        Employee result = SUT.findEmployeeById(idInt);
-
-        id = String.valueOf(id);
-
-        // Assert
-        assertNotNull(result);
-    }
-
-    @Test
-    void TS_4_TC_1_find_Employee_by_id_with_invalid_id() {
-        // Arrange
-        EmployeeDAO SUT = new EmployeeDAO(ConnectionEnvironment.TESTING);
-        int id = 2;
-
-        Employee result = SUT.findEmployeeById(id);
-
-        // Assert
-        assertNull(result);
-    }
-
-    @Test
-    void TS_4_TC_1_find_Employee_by_id_with_id_null() {
-        // Arrange
-        EmployeeDAO SUT = new EmployeeDAO(ConnectionEnvironment.TESTING);
-        int id = 0;
-
-        // Act
-        Employee result = SUT.findEmployeeById(id);
-
-        // Assert
-        assertNull(result);
-    }
-
+    
     @AfterEach
     void tearDown() {
         Connection connection = DBConnection.getInstance(ConnectionEnvironment.TESTING).getConnection();
@@ -100,8 +63,42 @@ class TestEmployeeDAO {
         }
     }
 
+    @Test
+    void TS_4_TC_1_find_Employee_by_id_with_valid_id() {
+    	
+        // Arrange
+        EmployeeDAO SUT = new EmployeeDAO(ConnectionEnvironment.TESTING);
 
+        // Act
+        Employee result = SUT.findEmployeeById(1);
 
+        // Assert
+        assertNotNull(result);
+    }
+
+    @Test
+    void TS_4_TC_1_find_Employee_by_id_with_invalid_id() {
+    	
+        // Arrange
+        EmployeeDAO SUT = new EmployeeDAO(ConnectionEnvironment.TESTING);
+
+        Employee result = SUT.findEmployeeById(2);
+
+        // Assert
+        assertNull(result);
+    }
+
+    @Test
+    void TS_4_TC1_find_Employee_by_id_with_id_null() {
+    	
+        // Arrange
+        EmployeeDAO SUT = new EmployeeDAO(ConnectionEnvironment.TESTING);
+
+        // Act
+        Employee result = SUT.findEmployeeById(0);
+
+        // Assert
+        assertNull(result);
+        
+    }    
 }
-
-
