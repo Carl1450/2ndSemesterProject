da9672882567093c2e4e5076b4b6a4441338c4e2
nikolinehalbak <128796372+nikoUCN@users.noreply.github.com>
2023-12-11 11:56:19 +0100

    OrderDAO ændringer

--- 2ndSemesterProject/src/Database/OrderDAO.java
+++ 2ndSemesterProject/src/Database/OrderDAO.java
@@ -1,6 +1,5 @@
 package Database;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -8,39 +7,60 @@ import java.util.ArrayList;
 
 import Model.Order;
 import Model.OrderLine;
+import Model.Product;
 
 public class OrderDAO {
 
-    private ConnectionEnvironment env;
-    String insertOrderQ = "INSERT INTO Order(date, totalPrice, employeeId) VALUES (?, ?, ?);";
-    String insertOrderLineQ = "INSERT INTO OrderLine(orderId, productId, quantity) VALUES (?, ?, ?);";
-    
-    public OrderDAO(ConnectionEnvironment env) {
-        this.env = env;
-        
-    }
-    
-    public boolean saveOrder(Order order) throws SQLException {
-    	Connection connection = DBConnection.getConnection(env);
-    	try(PreparedStatement prepStat = connection.prepareStatement(insertOrderQ)) {
-    		prepStat.setDate(1, order.getDate());
-    		prepStat.setFloat(2, order.getTotalPrice());
-    		prepStat.setInt(3, order.getEmployee().getId());
-    		
-    		int result = prepStat.executeUpdate();
-    		
-    		boolean orderInserted = false;
-    		if(result > 0) {
-    			orderInserted = true;
-    			//saveOrderlines(order.getOrderLines());
-    		}
-            return orderInserted;
-    	}    	
-    }
-    
-    public boolean saveOrderLines(Connection connection, ArrayList<OrderLine> orderLines) {
-    	
-		return false;
-    	
-    }
+	private ConnectionEnvironment env;
+	private Product product;
+	String insertOrderQ = "INSERT INTO Order(date, totalPrice, employeeId) VALUES (?, ?, ?);";
+	String insertOrderLineQ = "INSERT INTO OrderLine(orderId, productId, quantity) VALUES (?, ?, ?);";
+
+	public OrderDAO(ConnectionEnvironment env) {
+		this.env = env;
+	}
+
+	public boolean saveOrder(Order order) throws SQLException {
+		Connection connection = DBConnection.getConnection(env);
+		try (PreparedStatement prepStat = connection.prepareStatement(insertOrderQ)) {
+			prepStat.setDate(1, order.getDate());
+			prepStat.setFloat(2, order.getTotalPrice());
+			prepStat.setInt(3, order.getEmployee().getId());
+
+			int result = prepStat.executeUpdate();
+
+			boolean orderInserted = false;
+			if (result > 0) {
+				orderInserted = true;
+				try (var generatedKeys = prepStat.getGeneratedKeys()) {
+					if (generatedKeys.next()) {
+						int orderId = generatedKeys.getInt(1);
+						saveOrderLines(order.getOrderLines(), orderId);
+					}
+				}
+			}
+			return orderInserted;
+		}
+	}
+
+	public boolean saveOrderLines(ArrayList<OrderLine> orderLines, int orderId) throws SQLException {
+		try (Connection connection = DBConnection.getConnection(env);) {
+			PreparedStatement prepStat = connection.prepareStatement(insertOrderLineQ);
+
+			boolean orderLineInserted = false;
+
+			for (OrderLine orderLine : orderLines) {
+				prepStat.setInt(1, orderId);
+				prepStat.setInt(2, product.getBarcode());
+				prepStat.setInt(3, orderLine.getQuantity());
+				int result = prepStat.executeUpdate();
+
+				if (result > 0) {
+					orderLineInserted = true;
+				}
+			}
+			return orderLineInserted;
+		}
+
+	}
 }
