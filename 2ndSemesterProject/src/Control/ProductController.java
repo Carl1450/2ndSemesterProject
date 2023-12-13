02726eb45bd73f6f95e1bb24eeffbae0711ea3d5
RadenDK <128796441+RasmusJensenUCN@users.noreply.github.com>
2023-12-13 15:59:06 +0100

    USE CASE CRUD Product working

--- 2ndSemesterProject/src/Control/ProductController.java
+++ 2ndSemesterProject/src/Control/ProductController.java
@@ -4,22 +4,68 @@ import Database.ConnectionEnvironment;
 import Database.ProductDAO;
 import Model.Product;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductController {
-	
-	private ProductDAO productDAO;
+
+    private ProductDAO productDAO;
+
+    private List<Product> allProducts;
 
     private ConnectionEnvironment env;
+
     public ProductController(ConnectionEnvironment env) {
         this.env = env;
         productDAO = new ProductDAO(env);
+        allProducts = new ArrayList<>();
     }
-    
+
     public Product findProductByBarcode(int barcode) {
-    	return productDAO.findProductByBarcode(barcode);
+        return productDAO.findProductByBarcode(barcode);
     }
-    
+
+    public List<Product> findProductsStartingWith(String barcodeStart, boolean retrieveNewData) {
+        if (allProducts.isEmpty() || retrieveNewData) {
+            allProducts = productDAO.getAllProducts();
+        }
+
+        List<Product> productsStartingWith = new ArrayList<>();
+
+
+        for (Product product : allProducts) {
+            String barcodeString = Integer.toString(product.getBarcode()).trim();
+            if (barcodeString.equals("") || barcodeString.startsWith(barcodeStart)) {
+                productsStartingWith.add(product);
+            }
+
+        }
+
+        return productsStartingWith;
+    }
+
+
     public boolean saveProduct(int barcode, String name, int stockNumber) {
-    	return productDAO.saveProduct(barcode, name, stockNumber);
+        return productDAO.saveProduct(barcode, name, stockNumber);
+    }
+
+    public boolean updateProduct(int oldBarcode, String newBarcode, String newName, String newStockNumber) {
+        return productDAO.updateProduct(oldBarcode, Integer.parseInt(newBarcode), newName, Integer.parseInt(newStockNumber));
+    }
+
+    public boolean deleteProduct(Product product) {
+
+        boolean success = false;
+
+        try {
+            success = productDAO.deleteProduct(product);
+
+        } catch (RuntimeException e) {
+            System.out.println("A conflict in the database occurred, Product can not be deleted");
+        }
+
+        return success;
     }
 
 }
--- 2ndSemesterProject/src/GUI/ProductTableModel.java
+++ 2ndSemesterProject/src/GUI/ProductTableModel.java
@@ -5,90 +5,82 @@ import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
-import Model.OrderLine;
 import Model.Price;
+import Model.Product;
 
 public class ProductTableModel extends AbstractTableModel {
 
-	private List<OrderLine> data;
-	private static final String[] COL_NAMES = { "Product Name", "Barcode", "Quantity", "Price" };
+    private List<Product> data;
+    private static final String[] COL_NAMES = { "Product Name", "Barcode", "Quantity", "Price" };
 
-	public ProductTableModel(List<OrderLine> orderLines) {
-		super();
+    public ProductTableModel(List<Product> products) {
+        super();
 
-		if (orderLines != null) {
-			this.data = orderLines;
-		} else {
-			this.data = new ArrayList<>();
-			System.out.println("Warning: Product list was null. Initializing with an empty list.");
-		}
-	}
+        if (products != null) {
+            this.data = products;
+        } else {
+            this.data = new ArrayList<>();
+            System.out.println("Warning: Product list was null. Initializing with an empty list.");
+        }
+    }
 
-	@Override
-	public String getColumnName(int col) {
-		return COL_NAMES[col];
-	}
+    @Override
+    public String getColumnName(int col) {
+        return COL_NAMES[col];
+    }
 
-	@Override
-	public int getRowCount() {
-		return data.size();
-	}
+    @Override
+    public int getRowCount() {
+        return data.size();
+    }
 
-	@Override
-	public int getColumnCount() {
-		return COL_NAMES.length;
-	}
+    @Override
+    public int getColumnCount() {
+        return COL_NAMES.length;
+    }
 
-	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		OrderLine o = data.get(rowIndex);
-		String res = "";
-		if (o != null) {
-			switch (columnIndex) {
-			case 0:
-				String name = o.getProduct().getName();
-				res = name;
-				break;
-			case 1:
-				int barcode = o.getProduct().getBarcode();
-				res = String.valueOf(barcode);
-				break;
-			case 2:
-				int quantity = o.getQuantity();
-				res = String.valueOf(quantity);
-				break;
-			case 3:
-				if (o.getQuantity() > 1) {
-					Price price = o.getProduct().getPrice();
-					float totalPrice = price.getPrice() * o.getQuantity();
-					res = String.valueOf(totalPrice);
-				} else {
-					Price price = o.getProduct().getPrice();
-					float singlePrice = price.getPrice();
-					res = String.valueOf(singlePrice);
-				}
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Product product = data.get(rowIndex);
+        String res = "";
+        if (product != null) {
+            switch (columnIndex) {
+                case 0:
+                    int barcode = product.getBarcode();
+                    res = String.valueOf(barcode);
+                    break;
+                case 1:
+                    String name = product.getName();
+                    res = name;
+                    break;
+                case 2:
+                    int stockNumber = product.getStockNumber();
+                    res = String.valueOf(stockNumber);
+                    break;
+                case 3:
+                    Price price = product.getPrice();
+                    res = String.valueOf(price.getPrice());
+                    break;
+                default:
+                    res = "Unknown";
+            }
+        } else {
+            res = "N/A";
+        }
+        return res;
 
-				break;
-			default:
-				res = "Unknown";
-			}
-		} else {
-			res = "N/A";
-		}
-		return res;
+    }
 
-	}
+    public Product getProduct(int index) {
+        return data.get(index);
+    }
 
-	public OrderLine getOrderLine(int index) {
-		return data.get(index);
-	}
+    public void setData(List<Product> data) {
+        this.data = data;
+        super.fireTableDataChanged();
+    }
 
-	public void setData(List<OrderLine> data) {
-		this.data = data;
-		super.fireTableDataChanged();
-	}
-
-	public List<OrderLine> getData(){
-		return data;
-	}
+    public List<Product> getData(){
+        return data;
+    }
 }
