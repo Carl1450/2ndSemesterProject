60ed5fa1c4d55fa1ba20b80ff1d4c884b42b0f1d
RadenDK <128796441+RasmusJensenUCN@users.noreply.github.com>
2023-12-14 15:32:35 +0100

    Employee backend

--- 2ndSemesterProject/src/Database/EmployeeDAO.java
+++ 2ndSemesterProject/src/Database/EmployeeDAO.java
@@ -1,9 +1,6 @@
 package Database;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,6 +41,225 @@ public class EmployeeDAO {
         }
         return employee;
     }
+
+    public List<Employee> getAllEmployees() {
+
+        List<Employee> employees = new ArrayList<>();
+
+        String findAllEmployeesQuery = "SELECT emp.id, emp.fname, emp.lname, emp.password, emp.[role], emp.phoneno, emp.email, " +
+                "[address].street, [address].streetno, [address].zipcode, city.city " +
+                "FROM Employee emp " +
+                "LEFT JOIN [address] ON emp.addressId = [address].id " +
+                "LEFT JOIN city ON [address].zipcode = city.zipcode ";
+
+        Connection connection = DBConnection.getConnection(env);
+        ResultSet resultSet = DBConnection.executeQuery(connection, findAllEmployeesQuery);
+
+        try {
+            while (resultSet.next()) {
+                employees.add(EmployeeFactory.getEmployee(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return employees;
+    }
+
+
+
+    public boolean saveEmployee(String name, String address, String phoneNumber, String email, int zipCode,
+                                String city, String role, String cprNo, String password) {
+
+        int rowsAffected = 0;
+
+        String insertEmployee = "INSERT INTO Employee (fname, lname, email, phoneno, addressId, [role], cprNo, [password]) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        Connection connection = DBConnection.getConnection(env);
+
+        String[] splitName = name.split("\\s+");
+        String[] splitAddress = address.split("\\s+");
+
+        try {
+            DBConnection.startTransaction(connection);
+
+            boolean savedCity = saveCity(city, zipCode);
+            if (!savedCity) {
+                savedCity = updateCity(city, zipCode);
+            }
+
+            int addressId = saveAddress(splitAddress[0], Integer.parseInt(splitAddress[1]), zipCode);
+
+            PreparedStatement preparedStatement = connection.prepareStatement(insertEmployee);
+            preparedStatement.setNString(1, splitName[0]);
+            preparedStatement.setNString(2, splitName[1]);
+            preparedStatement.setNString(3, email);
+            preparedStatement.setNString(4, phoneNumber);
+            preparedStatement.setInt(5, addressId);
+            preparedStatement.setNString(6, role);
+            preparedStatement.setNString(7, cprNo);
+            preparedStatement.setNString(8, password);
+
+            rowsAffected = preparedStatement.executeUpdate();
+
+            if (savedCity && rowsAffected > 0) {
+                DBConnection.commitTransaction(connection);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                DBConnection.rollbackTransaction(connection);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        } finally {
+            DBConnection.closeConnection(connection);
+        }
+
+        return rowsAffected > 0;
+    }
+
+    private boolean saveCity(String city, int zipcode) {
+        int rowsAffected = 0;
+
+        String insertCityQ = "INSERT INTO City( city, zipcode) VALUES (?, ?);";
+
+        Connection connection = DBConnection.getConnection(env);
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(insertCityQ);
+            preparedStatement.setNString(1, city);
+            preparedStatement.setInt(2, zipcode);
+
+            rowsAffected = preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            if (e.getSQLState().equals("23000") && e.getErrorCode() == 2627) {
+                System.err.println("Primary key violation: The record with the specified primary key already exists.");
+            } else {
+                e.printStackTrace();
+            }
+        }
+
+        DBConnection.closeConnection(connection);
+
+        return rowsAffected > 0;
+    }
+
+    public boolean updateCity(String city, int zipCode) {
+
+        int rowsAffected = 0;
+
+
+        return rowsAffected > 0;
+
+    }
+
+    private int saveAddress(String street, int streetno, int zipcode) {
+        int rowsAffected = 0;
+
+        int addressID = -1;
+
+        String insertAddressQ = "INSERT INTO Address(street, streetno, zipcode) VALUES (?, ?, ?);";
+
+        Connection connection = DBConnection.getConnection(env);
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(insertAddressQ, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setNString(1, street);
+            preparedStatement.setInt(2, streetno);
+            preparedStatement.setInt(3, zipcode);
+
+            rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        addressID = generatedKeys.getInt(1);
+
+                    } else {
+                        System.err.println("Failed to retrieve the generated ID for the address.");
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBConnection.closeConnection(connection);
+
+        }
+
+        return addressID;
+
+    }
+
+    public boolean UpdateEmployee(int employeeId, String name, String address, String phoneNumber, String email, int zipCode,
+                                  String city, String role) {
+        int rowsAffected = 0;
+
+        String updateEmployeeQuery = "UPDATE Employee SET fname = ?, SET lname = ?, SET email = ?, SET phoneno = ?, SET AddressId = ?, SET [role] = ? WHERE id = ?;";
+
+        Connection connection = DBConnection.getConnection(env);
+
+
+        String[] splitName = name.split("\\s+");
+        String[] splitAddress = address.split("\\s+");
+
+        try {
+            DBConnection.startTransaction(connection);
+
+            boolean updatedCity = updateCity(city, zipCode);
+
+            int addressId = saveAddress(splitAddress[0], Integer.parseInt(splitAddress[1]), zipCode);
+
+            PreparedStatement preparedStatement = connection.prepareStatement(updateEmployeeQuery);
+            preparedStatement.setNString(1, splitName[0]);
+            preparedStatement.setNString(2, splitName[1]);
+            preparedStatement.setNString(3, email);
+            preparedStatement.setNString(4, phoneNumber);
+            preparedStatement.setInt(5, addressId);
+            preparedStatement.setNString(6, role);
+            preparedStatement.setInt(7, employeeId);
+
+            rowsAffected = preparedStatement.executeUpdate();
+
+            DBConnection.commitTransaction(connection);
+
+        } catch (SQLException e) {
+            try {
+                DBConnection.rollbackTransaction(connection);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        } finally {
+            DBConnection.closeConnection(connection);
+        }
+
+        return rowsAffected > 0;
+    }
+
+
+    public boolean deleteEmployee(int employeeId) {
+        String deleteCustomerQ = "DELETE FROM Employee WHERE id = =";
+        int rowsAffected = 0;
+
+        try (Connection connection = DBConnection.getConnection(env);
+             PreparedStatement preparedStatement = connection.prepareStatement(deleteCustomerQ)) {
+
+            preparedStatement.setInt(1, employeeId);
+            rowsAffected = preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return rowsAffected > 0;
+
+    }
+
 
     public List<Janitor> getAllJanitors() {
         List<Janitor> janitors = new ArrayList<>();
